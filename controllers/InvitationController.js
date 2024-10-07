6a23dc976ab70a518e3ab37e0afa7eaae6bae772
Jarlarex <jarla1335@gmail.com>
2024-10-07 18:17:13 +0100

    Add InvitationController to create invitations and generate invitation URLs

---
+++ controllers/InvitationController.js
@@ -0,0 +1,23 @@
+const Invitation = require('../models/invitation');
+
+exports.createInvitation = async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        // Create a new invitation with a unique code
+        const invitation = new Invitation({ email });
+
+        await invitation.save();
+
+        // Generate the invitation url
+        const invitationUrl = `${req.protocol}://${req.get('host')}/register/${invitation.uniqueCode}`;
+
+        res.status(201).json({
+            message: 'Invitation created successfully',
+            invitationUrl,
+        });
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to create invitation' });
+    }
+};
