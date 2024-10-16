41e1256530911fc81c6877e8a8ff04039ac4bcbe
Jarlarex <jarla1335@gmail.com>
2024-10-16 13:10:20 +0100

    Refactor registration form and validation in RegistrationForm.vue

--- admin/src/views/HomeView.vue
+++ admin/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { onMounted } from "vue";
 import SignUp from "@/components/SignOptions.vue";
-import {useSignUpStore} from "@/stores/signUpStore.js";
+import { useSignUpStore } from "@/stores/signUpStore.js";
 import HotelRegistration from "@/components/HotelRegistration.vue";
 import RegistrationForm from "@/components/RegistrationForm.vue";
 
@@ -15,19 +15,21 @@ const checkForVerificationCode = () => {
     signUpStore.goTo(signUpStore.signUpPagesEnum.REGISTER);
     signUpStore.setVerificationCode(code);
   }
+};
 
-  onMounted(() => {
-    checkForVerificationCode();
-  });
-}
-
+onMounted(() => {
+  checkForVerificationCode();
+});
 </script>
 
 <template>
   <main>
-    <div class="move-back" v-if="signUpStore.currentPage !== signUpStore.signUpPagesEnum.WELCOME" @click="signUpStore.goTo(signUpStore.signUpPagesEnum.WELCOME)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-arrow-left" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"></path>
-    </svg>Back</div>
+    <div class="move-back" v-if="signUpStore.currentPage !== signUpStore.signUpPagesEnum.WELCOME" @click="signUpStore.goTo(signUpStore.signUpPagesEnum.WELCOME)">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-arrow-left" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"></path>
+      </svg>
+      Back
+    </div>
     <Transition name="fade" mode="out-in">
       <sign-up v-if="signUpStore.currentPage === signUpStore.signUpPagesEnum.WELCOME" />
     </Transition>
@@ -41,7 +43,7 @@ const checkForVerificationCode = () => {
 </template>
 
 <style scoped>
-.move-back{
+.move-back {
   position: absolute;
   right: 20px;
   top: 20px;
@@ -55,11 +57,11 @@ const checkForVerificationCode = () => {
   padding: 8px 10px;
 }
 
-.move-back:hover{
+.move-back:hover {
   background-color: #555555;
 }
 
-.move-back svg{
+.move-back svg {
   margin-right: 10px;
 }
 </style>
